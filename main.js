0a272a2f341632ea0a324c818c7c785c85685f2f
Anton Yadchenko <antonfreedom5@gmail.com>
2024-01-28 21:12:26 +0300

    change endpoints

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ bot.command('start', (ctx) => {
         );
 })
 
-bot.on(message('web_app_data'), async (ctx) => {
-    const data = ctx.webAppData.data.json()
-    ctx.reply(`Ваше сообщение: ${data}` ?? 'empty message')
-})
+// bot.on(message('web_app_data'), async (ctx) => {
+//     const data = ctx.webAppData.data.json()
+//     ctx.reply(`Ваше сообщение: ${data}` ?? 'empty message')
+// })
 
 bot.on('callback_query', async (ctx) => {
     const id = ctx.update.callback_query.data;
@@ -30,19 +30,19 @@ bot.on('callback_query', async (ctx) => {
     const from = ctx.update.callback_query.from;
     const currentOrder = order;
     console.log(message.chat.type);
-    ctx.editMessageText({chat_id: -4170265159, message_id: message.message_id, text: message.text + '\n В работу взял ' + from.first_name, reply_markup: null});
+    ctx.editMessageText({chat_id: process.env.GROUP_ID, message_id: message.message_id, text: message.text + '\n В работу взял ' + from.first_name, reply_markup: null});
     await bot.telegram.sendMessage(from.id, 'Ура! У тебя новый заказ!\n ' + currentOrder.phone + currentOrder.place);
 })
 
 bot.launch();
 
-const app = new Express();
+const app = Express();
 
 app.use(BodyParser.json());
 
 app.use(BodyParser.urlencoded({ extended: true }));
 
-app.post("/post", (req, res) => {
+app.post("/", (req, res) => {
     const { machineId, attachmentId, phone, place, date } = req.body;
     const id = 1;
     order = { id, machineId, attachmentId, phone, place, date }
@@ -50,10 +50,6 @@ app.post("/post", (req, res) => {
         Markup.button.callback('Беру заказ', id.toString())
     ]))
     res.json({ message: "Это стартовая страница нашего приложения" + req.body.user });
-});
-
-app.get("/", (req, res) => {
-    res.json({ message: "Это стартовая страница нашего приложения" });
 });
 
 app.listen();
